6398aabcf832d6a31446e840a36d557e2443b210
huangziyi01 <huangziyi01@baidu.com>
2016-02-02 20:45:03 +0800

    quick push

--- src/scroll-listen-container/index.js
+++ src/scroll-listen-container/index.js
@@ -26,51 +26,69 @@ export default class ScrollListenContainer extends React.Component {
         this.refs['box'].scrollTo(key)
     }
 
+    // 递归生成children
+    createChildren(children, findTree, index = 0) {
+        console.log(children)
+        return React.Children.map(children, (item, itemIndex)=> {
+            if (item.props && item.props.children) {
+                //return React.cloneElement(children, children.props, this.createChildren(item.props.children, findTree, itemIndex + 1))
+                let cc = this.createChildren(item.props.children, findTree, itemIndex + 1)
+                if (cc.type===undefined){
+                    console.log(children)
+                }
+                return React.cloneElement(children, children.props, children.props.children)
+            } else if (item.props && !item.props.children) {
+                return React.cloneElement(children, children.props)
+            } else {
+                return React.cloneElement(children)
+            }
+        })
+        // 如果不是最后一个,直接往下递归
+        //if (index !== findTree.length - 1) {
+        //    console.log('数组', children)
+        //    return React.cloneElement(children)
+        //} else {
+        //    console.log('到了!')
+        //}
+    }
+
     render() {
         let boxFindTree = findTreeByName(this.props.children, 'ScrollListenBox')
         let listenFindTree = findTreeByName(this.props.children, 'ScrollListen')
-
         let boxChildren = getTargetByTree(boxFindTree)
-        let listenChildren = getTargetByTree(listenFindTree)
+        let nailFindTree = findTreeByName(boxChildren.props.children, 'ScrollListenNail')
 
         // 生成titles
         let titles = []
-        let nailFindTree = findTreeByName(boxChildren.props.children, 'ScrollListenNail')
-        console.log(nailFindTree)
-
-        //React.Children.map(item.props.children, (nailItem, index)=> {
-        //    if (nailItem.type.name !== 'ScrollListenNail')return
-        //    titles.push({
-        //        key: index,
-        //        name: nailItem.props.title
-        //    })
-        //})
-
-
-        //let MergedChildren = this.findTree.map((children)=> {
-        //    let props = Object.assign({}, children.props)
-        //    return React.cloneElement(children, props)
-        //})
-
-        //console.log(this.findTree)
-        //this.getChildrenByName(this.props.children, 'ScrollListen')
-        //console.log(this.ScrollListenBox)
-
-
-        let Children = React.Children.map(this.props.children, (item)=> {
-            let props = Object.assign({}, item.props)
-            if (item.type.name === 'ScrollListenBox') {
-                props.onScrollKeyChange = this.handleScrollKeyChange.bind(this)
-                props.ref = 'box'
-                props.titles = titles
-            }
-            if (item.type.name === 'ScrollListen') {
-                props.onChange = this.handleClickKeyChange.bind(this)
-                props.activeKey = this.state.key
-                props.titles = titles
-            }
-            return React.cloneElement(item, props)
+        // 找到上一级
+        if (nailFindTree.length >= 2) {
+            nailFindTree.pop()
+        }
+        let nailParent = getTargetByTree(nailFindTree)
+        React.Children.map(nailParent.props.children, (nailItem, index)=> {
+            if (nailItem.type.name !== 'ScrollListenNail')return
+            titles.push({
+                key: index,
+                name: nailItem.props.title
+            })
         })
+
+        let Children = this.createChildren(this.props.children, boxFindTree)
+
+        //let Children = React.Children.map(this.props.children, (item)=> {
+        //    let props = Object.assign({}, item.props)
+        //    if (item.type.name === 'ScrollListenBox') {
+        //        props.onScrollKeyChange = this.handleScrollKeyChange.bind(this)
+        //        props.ref = 'box'
+        //        props.titles = titles
+        //    }
+        //    if (item.type.name === 'ScrollListen') {
+        //        props.onChange = this.handleClickKeyChange.bind(this)
+        //        props.activeKey = this.state.key
+        //        props.titles = titles
+        //    }
+        //    return React.cloneElement(item, props)
+        //})
 
         return (
             <div {...this.props}>{Children}</div>
